86cc3fb3db5aff61de10f3318a674c67b96d336c
Jen E <jen@wecancodeit.org>
2019-10-29 13:05:46 -0400

    workshop 1 initial commit

---
+++ backend/todos/todos.Tests/TodoControllerTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace todos.Tests
+{
+    public class TodoControllerTests
+    {
+        [Fact]
+        public void Get_Returns_List_of_Todos()
+        {
+
+        }
+
+        [Fact]
+        public void Post_Creates_New_Todo()
+        {
+
+        }
+    }
+}
---
+++ backend/todos/todos/Controllers/TodoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace todos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        // GET api/todos
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "todo1", "todo2" };
+        }
+
+        // GET api/todos/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "todo";
+        }
+
+        // POST api/todos
+        [HttpPost]
+        public void Post([FromBody] string todo)
+        {
+        }
+
+        // PUT api/todos/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string todo)
+        {
+        }
+
+        // DELETE api/todos/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
